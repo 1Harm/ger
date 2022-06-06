75c87294570c7ea0f6f7817a3ee3854408346f6f
1Harm <ghost9029@mail.ru>
2022-06-06 20:24:43 +0600

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const  express = require('express')
 const app = express()
-var createError = require('http-errors');
 const UserRoute = require('./routes/UserRoute')
 const methodOverride = require('method-override')
 app.use('/user',UserRoute)
@@ -16,18 +15,18 @@ const port = 7777
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs')
 app.use("/", require("./re"));
+app.use("/go", require("./go"));
 app.use("/login", require("./login"));
 app.use("/google", require("./routes/google"));
 app.use("/about", require("./abouts"));
 app.use("/prof", require("./prof"));
 app.use("/au", require("./routes/au"));
 app.use("/deleteds", require("./routes/deleteds"));
-app.use('/ad', require("./routes/index"));
-app.use('/users', require("./routes/users"));
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 );
-
+const { MongoClient } = require('mongodb');
+const{ ObjectId } = require('mongodb').ObjectId;
 const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://1Harm:EsOd9029@cluster0.7hrdf.mongodb.net/Signin');
 let db = mongoose.connection;
@@ -36,42 +35,6 @@ db.once('open', function(callback){
     console.log("connection succeeded");
 })
 const ejs=require('ejs')
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-const config = require('./config');
-const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors');
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
-app.use((req, res, next) => {
-  const collection = req.app.locals[config.dbCollection];
-  req.collection = collection;
-  next();
-});
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-
 app.post('/sign_up', function(req,res){
     var email =req.body.email;
     var password = req.body.password;
@@ -105,7 +68,9 @@ app.get('/google/callback',
 app.get('/google/failure', (req,res)=>{
     res.send('Something went wrong')
 })
-
+app.get('/doctor',((req, res) => {
+  res.sendFile(__dirname+'/doctor.html')
+}))
 app.get('/auth/google',
     passport.authenticate('google', {scope:['email', 'profile']}))
 app.get('/protected', isLoggedIn, (req, res) => {
@@ -128,3 +93,107 @@ app.get('/update', (req, res) => {
 app.get('/delete', (req, res) => {
     res.render('delete');
 });
+app.post('/los', function(req,res){
+  var email =req.body.email;
+  var password = req.body.password;
+  var data = {
+    "email":email,
+    "password":password
+  }
+  db.collection('Doctors').insertOne(data,function(err, collection){
+    if (err) throw err;
+    console.log("Record inserted Successfully");
+
+  });
+
+  return  res.sendFile(__dirname+'/queue.html');
+})
+async function main() {
+  const uri = "mongodb+srv://user:aisha@cluster0.ppulr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  try {
+
+    await client.connect();
+
+    await init(client);
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+main().catch(console.err);
+
+
+async function init(client) {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true })); //extended:true to encode objects and arrays  https://github.com/expressjs/body-parser#bodyparserurlencodedoptions
+
+  const db = client.db('eventList')
+  const events = db.collection('events')
+
+  app.get('/init', function (req, res) {
+    events.insertOne({
+      text: "Some Helpful event",
+      start_date: new Date(2018, 8, 1),
+      end_date: new Date(2018, 8, 5)
+    })
+    events.insertOne({
+      text: "Another Cool Event",
+      start_date: new Date(2018, 8, 11),
+      end_date: new Date(2018, 8, 11)
+    })
+    events.insertOne({
+      text: "Super Activity",
+      start_date: new Date(2018, 8, 9),
+      end_date: new Date(2018, 8, 10)
+    })
+    events.save();
+    res.send("Test events were added to the database")
+  });
+
+  app.get('/data', function (req, res) {
+    events.find().toArray(function (err, data) {
+      //set the id property for all client records to the database records, which are stored in ._id field
+      for (var i = 0; i < data.length; i++){
+        data[i].id = data[i]._id;
+        delete data[i]["!nativeeditor_status"];
+      }
+      //output response
+      res.send(data);
+    });
+  });
+
+
+  // Routes HTTP POST requests to the specified path with the specified callback functions. For more information, see the routing guide.
+  // http://expressjs.com/en/guide/routing.html
+
+  app.post('/data', function (req, res) {
+    var data = req.body;
+    var mode = data["!nativeeditor_status"];
+    var sid = data.id;
+    var tid = sid;
+
+    function update_response(err) {
+      if (err)
+        mode = "error";
+      else if (mode === "inserted"){
+        tid = data._id;
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.send({ action: mode, sid: sid, tid: String(tid) });
+    }
+
+    if (mode === "updated") {
+      events.updateOne({"_id": ObjectId(tid)}, {$set: data}, update_response);
+    } else if (mode === "inserted") {
+      events.insertOne(data, update_response);
+    } else if (mode === "deleted") {
+      events.deleteOne({"_id": ObjectId(tid)}, update_response)
+    } else
+      res.send("Not supported operation");
+  });
+};
